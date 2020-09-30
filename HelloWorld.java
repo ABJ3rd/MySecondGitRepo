b4059fe386f1cafe3bc5ffe336ffc69821d8eba2
Andrew Jordan <ABJ3rd@suddenlink.net>
2020-09-30 10:29:37 -0500

    This is my Second Git Commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("This is my Second java program on Linux");
+		System.out.println("My second commit on Linux");
 	}
 }
